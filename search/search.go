b03bf98628117a3fe6b5d3164c502a20c82c319a
William Kennedy <bill@ardanstudios.com>
2013-09-02 00:02:47 -0400

    Fixed reference to utilities

--- search/search.go
+++ search/search.go
@@ -12,7 +12,7 @@ import (
 	"github.com/goinggo/newssearch/helper"
 	"github.com/goinggo/newssearch/plugins/expression"
 	"github.com/goinggo/newssearch/rss"
-	"github.com/goinggo/utilities/v1.0.0/workpool"
+	"github.com/goinggo/utilities/v1/workpool"
 	"io"
 	"os"
 	"path/filepath"
